d514cafed30cc3dbbf13dc68124a1fd3d2fbe280
Elber <elberrd@gmail.com>
2022-09-18 07:49:11 -0400

    Seconds passed

--- styles/global.ts
+++ styles/global.ts
@@ -15,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
     body{
         background: ${(propos) => propos.theme["gray-900"]};
         color: ${(propos) => propos.theme["gray-300"]};
+        -webkit-font-smoothing: antialiased;
     }
     
     body, input, button, textarea{
